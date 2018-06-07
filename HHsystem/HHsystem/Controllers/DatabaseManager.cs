210960f1720c6381f5098083888a2170c18061f7
Unknown <swabe@live.co.za>
2018-06-07 17:05:30 +0200

    Update
    -Changed Xamarian File
    -Added database connection to PC version

---
+++ HHsystem/HHsystem/Controllers/DatabaseManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace HHsystem.Controllers
+{
+    class DatabaseManager
+    {
+        MySqlConnection conn;
+
+       public void connection() {
+            conn = new MySqlConnection("Server=localhost; database=HairSalonDB; UID=root; password=root");
+            conn.Open();
+        }
+
+        public object[][] bookings() {
+            object[][] member=null;
+            string commandString = "SELECT * FROM booking";
+            MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = commandString;
+            MySqlDataReader result = cmd.ExecuteReader();
+
+            int i=0;
+
+            if (result != null)
+            {
+                while (result.Read())
+                {
+                    member[i][0] = result["idBooking"].ToString();
+                    member[i][1] = result["idCustomer"].ToString();
+                    member[i][2] = result["idEmployee"].ToString();
+                    i++;
+                    Console.WriteLine(member[i][0]);
+                }
+            }
+
+            return member;
+        }
+    }
+}
---
+++ HHsystem/HHsystem/Forms/MainForm.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace HHsystem
+{
+    public partial class MainForm : Form
+    {
+        MySqlDataAdapter adapter;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            FillDataGridView();
+        }
+
+
+        public void populatetable()
+        {
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            bindingSource.DataSource = table;
+            dataGridView1.DataSource = bindingSource;
+        }
+
+        public void FillDataGridView()
+        {
+            try
+            {
+                MySqlConnection conn;
+                string command = "SELECT * FROM booking";
+                conn = new MySqlConnection("Server=localhost; database=HairSalonDB; UID=root; password=root");
+                conn.Open();
+
+                dataGridView1.AutoGenerateColumns = true;
+
+                MySqlCommand cmd = new MySqlCommand(command, conn);
+                MySqlDataAdapter adapter = new MySqlDataAdapter
+                {
+                    SelectCommand = cmd
+                };
+
+                DataTable table = new DataTable();
+
+                adapter.Fill(table);
+
+                bindingSource.DataSource = table;
+                dataGridView1.DataSource = bindingSource;
+
+    
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                throw;
+            }
+        }
+    }
+}
