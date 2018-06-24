c8e538866846b2244fb5fdec12caa7151bbecf0b
Unknown <swabe@live.co.za>
2018-06-24 22:28:44 +0200

    Hotfix
    Improved resizeability  of Daily Planner

--- HHsystem/HHsystem/Componets/CustomWeeklyScheduler.cs
+++ HHsystem/HHsystem/Componets/CustomWeeklyScheduler.cs
@@ -18,10 +18,11 @@ namespace HHsystem.Componets
             Initialize();
         }
 
-        public void addAppointment() {
-            cells[1,5].BackColor = System.Drawing.SystemColors.MenuHighlight;
+        public void addAppointment()
+        {
+            cells[1, 5].BackColor = System.Drawing.SystemColors.MenuHighlight;
             cells[1, 5].Text = "8:00 \n Mary";
-
+         
             cells[5, 1].BackColor = System.Drawing.SystemColors.MenuHighlight;
             cells[5, 1].Text = "8:00 \n Mary";
 
@@ -29,7 +30,8 @@ namespace HHsystem.Componets
             //cells[7, 7].Text = "8:00 \n Mary";
         }
 
-        public void getCurrentDate() {
+        public void getCurrentDate()
+        {
             DateTime startOfWeek = DateTime.Today.AddDays(
       (int)CultureInfo.CurrentCulture.DateTimeFormat.FirstDayOfWeek -
       (int)DateTime.Today.DayOfWeek);
@@ -51,12 +53,11 @@ namespace HHsystem.Componets
             x = (this.Height) / (rows + 1);
             yheader = (int)(y * 0.5);
             xheader = (int)(x * 0.5);
-            yInterval =y+(yheader/colums);
-            xInterval =x+(xheader/rows);
-
-
+            yInterval = y + (yheader / colums);
+            xInterval = x + (xheader / rows);
             int k = 0;
-
+            double errorCorrection = yInterval - (yheader * 2);
+           
             for (int j = 0; j <= rows; j++)
             {
                 for (int i = 0; i <= colums; i++)
@@ -71,36 +72,29 @@ namespace HHsystem.Componets
                         header.Size = new System.Drawing.Size(yheader, xheader);
                         this.Controls.Add(this.header);
                     }
-                    else if (j == 0 && i!=0)
+                    else if (j == 0 && i != 0)
                     {
                         header = new Label();
                         header.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-                        header.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+                       // header.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
                         header.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-                        header.Location = new System.Drawing.Point(((i * yInterval)-yheader), j * xInterval);
+                        header.Location = new System.Drawing.Point(((i * yInterval) - yheader - (int)errorCorrection), j * xInterval);
                         header.Name = "Header" + i;
                         header.Size = new System.Drawing.Size(yInterval, xheader);
                         header.Text = days[i];
-                        if (j == 0 && i==0)
-                        {
-                            header.Text = "";
-                           // header.Size = new System.Drawing.Size(yheader, xheader);
-                        }
-                        else {
-                            header.Text = myDates[k++].ToString("ddd dd/MM");
-                        }
+                        header.Text = myDates[k++].ToString("ddd dd/MM");
                         header.Font = new System.Drawing.Font("Arial", 11.25F);
                         header.TextAlign = ContentAlignment.TopCenter;
                         header.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
                         this.Controls.Add(this.header);
                     }
-                    else if (i == 0 && j!=0)
+                    else if (i == 0 && j != 0)
                     {
                         header = new Label();
                         header.BackColor = System.Drawing.SystemColors.ButtonHighlight;
                         header.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-                        header.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-                        header.Location = new System.Drawing.Point(i * y, (j * xInterval)-xheader);
+                   //     header.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+                        header.Location = new System.Drawing.Point(i * y, (j * xInterval) - xheader);
                         header.Name = "Header" + i;
                         header.Size = new System.Drawing.Size(yheader, xInterval);
                         header.Text = times[j];
@@ -116,12 +110,12 @@ namespace HHsystem.Componets
                         cell.BackColor = System.Drawing.SystemColors.ButtonFace;
                         cell.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
                         cell.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-                        cell.Location = new System.Drawing.Point(i*yInterval-yheader,j*xInterval-xheader);
+                        cell.Location = new System.Drawing.Point(i * yInterval - yheader- (int)errorCorrection, j * xInterval - xheader);
                         cell.Name = "Cell" + i;
                         cell.Size = new System.Drawing.Size(yInterval, xInterval);
                         cell.Text = "";
                         cell.Font = new System.Drawing.Font("Arial", 11.25F);
-                        cell.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+                        cell.ForeColor = System.Drawing.Color.White;
                         this.Controls.Add(this.cell);
                         cells[i, j] = cell;
                     }
@@ -134,7 +128,7 @@ namespace HHsystem.Componets
         private Label header;
         private Label cell;
         private String[] days = new String[] { "", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat", "Sun" };
-        private String[] times = new String[] { "","8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00" };
+        private String[] times = new String[] { "", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00" };
         private int y, x;
         private int yheader, xheader;
         private int yInterval, xInterval;
