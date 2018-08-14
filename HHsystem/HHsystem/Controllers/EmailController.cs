b759c5f292239ecfb7cd97dc3d418b3a2f085774
Andrew <swabe@live.co.za>
2018-08-14 22:27:27 +0200

    Minor Update
    -Added email template
    -Implemented email template

--- HHsystem/HHsystem/Controllers/EmailController.cs
+++ HHsystem/HHsystem/Controllers/EmailController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Mail;
 using System.Text;
@@ -15,6 +16,7 @@ namespace HHsystem.Controllers
         public void sendEmail(int id, string client, string employee, string date, string time)
         {
             String[,] details = manager.getClientDetails();
+            string body;
 
             string toAddess = "";
 
@@ -32,20 +34,30 @@ namespace HHsystem.Controllers
                 MailMessage mail = new MailMessage();
                 SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
 
-                mail.From = new MailAddress("eodding@gmail.com");
-                mail.To.Add("eric_odding@hotmail.com,swabe@live.co.za");
+                mail.From = new MailAddress("SP300Test@gmail.com");
+                mail.To.Add("eric_odding@hotmail.com, swabe@live.co.za");
                 mail.Subject = "Confirmation of Appointment for";
-                mail.Body = "Dear " + client + "\n\n" +
-                    "We hereby confirm your appointment at Heydt of Hair Design as follows:\n\n" +
-                    "Hairdresser: " + employee + "\n" +
-                    "Date: " + date + "\n" +
-                    "Time: " + time + "\n\n" +
-                    "If you wish to change your appointment, please contact us again.\n\n" + 
-                    "Kind regards\n" +
-                    "Heydt of Hair";
-
+                //mail.Body = "Dear " + client + "\n\n" +
+                //    "We hereby confirm your appointment at Heydt of Hair Design as follows:\n\n" +
+                //    "Hairdresser: " + employee + "\n" +
+                //    "Date: " + date + "\n" +
+                //    "Time: " + time + "\n\n" +
+                //    "If you wish to change your appointment, please contact us again.\n\n" + 
+                //    "Kind regards\n" +
+                //    "Heydt of Hair";
+                using (StreamReader reader = File.OpenText("C:\\Users\\Andrew\\Documents\\Projects\\HairSalonManagementSystem\\basic.html"))
+                {
+                    body = reader.ReadToEnd();
+                    
+                }
+                body = body.Replace("?@client", employee);
+                body =body.Replace("?@employee", employee);
+                body=body.Replace("?@date", date);
+                body=body.Replace("?@time", time);
+                mail.Body = body;
+                mail.IsBodyHtml = true;
                 SmtpServer.Port = 587;
-                SmtpServer.Credentials = new System.Net.NetworkCredential("eodding".Trim(), "Mainiak3#".Trim());
+                SmtpServer.Credentials = new System.Net.NetworkCredential("SP300Test".Trim(), "cTIgROUP5".Trim());
                 SmtpServer.EnableSsl = true;
 
                 SmtpServer.Send(mail);
